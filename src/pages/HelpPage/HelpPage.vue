56250d539485a8b9ede4cf4159053e5ceae62c6d
Julien Droulez <julien.droulez@cri-paris.org>
2023-10-18 16:35:15 +0200

    fix: add header to help page and fix bug on contact us form

--- src/pages/HelpPage/HelpPage.vue
+++ src/pages/HelpPage/HelpPage.vue
@@ -1,14 +1,56 @@
 <template>
     <div :class="{ loading: isLoading }" class="page-section-extra-wide help-layout">
+        <div>
+            <h1 class="page-title">{{ $filters.capitalize($t('faq.portal')) }}</h1>
+        </div>
+        <div class="intro-ctn">
+            <ul class="action-list">
+                <li>
+                    <a href="#" @click.prevent="showContactUsDrawer = true">{{
+                        $t('footer.contact')
+                    }}</a>
+                </li>
+                <li>
+                    <a href="#" @click.prevent="reportBugOpen = true">{{ $t('report.bug') }}</a>
+                </li>
+            </ul>
+        </div>
+
         <LpiLoader v-if="isLoading" type="simple" />
         <TabsLayout v-else :align-left="true" :border="false" :tabs="tabs"></TabsLayout>
     </div>
+    <DrawerLayout
+        :custom-style="customNotificationStyle"
+        :is-opened="showContactUsDrawer"
+        :has-footer="false"
+        class="medium"
+        confirm-action-name=""
+        :title="$t('footer.contact')"
+        @close="closeDrawer"
+    >
+        <ContactDrawer v-if="showContactUsDrawer" @close="closeDrawer" />
+    </DrawerLayout>
+    <DrawerLayout
+        v-if="reportBugOpen"
+        :confirm-action-name="$t('common.submit')"
+        :full-height="false"
+        :has-footer="false"
+        :is-opened="reportBugOpen"
+        :title="$t(`report.bug`)"
+        class="report-form"
+        @close="reportBugOpen = false"
+    >
+        <ReportForm type="bug" @close="reportBugOpen = false" />
+    </DrawerLayout>
 </template>
 
 <script>
 import TabsLayout from '@/components/lpikit/Tabs/TabsLayout.vue'
 import LpiLoader from '@/components/lpikit/Loader/LpiLoader.vue'
 import { defineAsyncComponent } from 'vue'
+import ReportForm from '@/components/lpikit/ReportForm/ReportForm.vue'
+import DrawerLayout from '@/components/lpikit/Drawer/DrawerLayout.vue'
+import ContactDrawer from '@/components/Drawers/ContactDrawer.vue'
 
 export default {
     name: 'HelpPage',
@@ -16,12 +58,21 @@ export default {
     components: {
         LpiLoader,
         TabsLayout,
+        ContactDrawer,
+        DrawerLayout,
+        ReportForm,
     },
 
     data() {
         return {
             isLoading: true,
             customTab: null,
+            showContactUsDrawer: false,
+            reportBugOpen: false,
+            customNotificationStyle: {
+                maxHeight: 'unset',
+                padding: 'unset',
+            },
         }
     },
 
@@ -77,11 +128,38 @@ export default {
 
             this.isLoading = false
         },
+        closeDrawer() {
+            this.showContactUsDrawer = false
+            this.reportBugOpen = false
+        },
     },
 }
 </script>
 
 <style lang="scss" scoped>
+.page-title {
+    text-align: center;
+    margin-bottom: $space-xl;
+    font-weight: 700;
+    font-size: $font-size-5xl;
+    line-height: 50px;
+    color: $black;
+    width: 100%;
+}
+.intro-ctn {
+    margin-bottom: $space-xl;
+    background: $primary-lighter;
+    padding: $space-l;
+    border-radius: $border-radius-17;
+    .action-list {
+        list-style-type: disc;
+        list-style-position: inside;
+        font-size: $font-size-l;
+        li {
+            padding: $space-m 0;
+        }
+    }
+}
 .help-layout {
     margin-top: calc($navbar-height + $space-m);
     display: flex;
