70c0fec497c2b586e936437a001ec790b530157f
Julien Droulez <julien.droulez@cri-paris.org>
2023-11-10 15:52:04 +0100

    fix: remove debug logs

--- src/components/lpikit/Tabs/TabsLayout.vue
+++ src/components/lpikit/Tabs/TabsLayout.vue
@@ -219,7 +219,6 @@ export default {
 
                 if (tabs.length) {
                     const wrapperRight = tabs[0]?.getBoundingClientRect()?.right
-                    console.log('wrapperRight', wrapperRight)
                     // reset the arrays
                     this.displayedTabs.splice(0)
                     this.seeMoreTabs.splice(0)
@@ -228,7 +227,6 @@ export default {
                     const iterate = async (tabs, displayed, otherTab) => {
                         let skipOthers = false
                         for (let i = 0; i < tabs.length; i++) {
-                            console.log('tabs[i]', i)
                             // add the tag to the displayed tags to compute its size
                             displayed.push(tabs[i])
                             await this.$nextTick()
@@ -239,10 +237,8 @@ export default {
                                 otherTab.push({ ...tabs[i], index: i })
                             } else if (tabsHtmlElement.length) {
                                 const lastTab = tabsHtmlElement[tabsHtmlElement.length - 1]
-                                console.log('lastTab', lastTab, lastTab.getBoundingClientRect())
                                 const lastTabRight = lastTab.getBoundingClientRect().right
                                 if (lastTabRight > wrapperRight) {
-                                    console.log('over')
                                     // if it doesn't, we hide it and add it to the more tags
                                     displayed.pop()
                                     otherTab.push({ ...tabs[i], index: i })
