77b8c4735fb38b5d83dcdf3db07952f9a9b6c880
Yohann Mepa <yohann.mepa@cri-paris.org>
2023-11-14 10:56:59 +0100

    remove comment

--- tests/playwright/lib/search.ts
+++ tests/playwright/lib/search.ts
@@ -5,16 +5,6 @@ import { LogLevel, Logger } from '../logger'
 const logger = new Logger(LogLevel.Debug)
 
 export async function search(page, projId) {
-    // await page.locator('[data-test="lpi-logo"]').click()
-    // logger.info('Click search input')
-    // logger.info('Fill it with project ID')
-    // await page.locator('[data-test="search-input"]').click()
-    // await page.locator('[data-test="search-input"]').fill(projId)
-    // logger.info('Wait 15 sec to load results')
-    // await delay(7000)
-    // logger.info('Click to launch the search')
-    // await page.locator('[data-test="search-input-button"]').click()
-    // await page.locator('[data-test^="project-card-"]').first().click()
     await page.locator('[data-test="lpi-logo"]').click()
     logger.info('Click search input')
     logger.info('Fill it with project ID')
