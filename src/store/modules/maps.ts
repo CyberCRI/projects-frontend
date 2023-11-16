b7a8a216e7ccf32f0a7726be7ce802ed170666c1
JulienDroulez <julien.droulez@cri-paris.org>
2023-11-16 11:39:17 +0100

    chore: remove map store and mixin (#114)

--- src/store/modules/maps.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    namespaced: true,
-    state: () => ({
-        attribution:
-            ' <a href="https://carto.com/basemaps/">Basemaps</a> | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>',
-    }),
-}
