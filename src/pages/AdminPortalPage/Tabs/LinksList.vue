48df07943a158a4e4a29d939f4461daeafe6f542
ymepacri <122785489+ymepacri@users.noreply.github.com>
2023-12-04 14:38:07 +0100

    feat:just link creation (#134)

--- src/pages/AdminPortalPage/Tabs/LinksList.vue
+++ src/pages/AdminPortalPage/Tabs/LinksList.vue
@@ -62,6 +62,7 @@
                                 class="sort-icon"
                                 :class="{ active: sortOn == 'expire_at' }"
                                 @click="sortLinks('expire_at')"
+                                data-test="sort-expire"
                             />
                         </th>
                         <th></th>
