d612c7ad766f52c21eb9522bb0f1d30dccbb87c3
Julien Droulez <julien.droulez@cri-paris.org>
2023-12-06 17:26:21 +0100

    feat: request access page (wip)

---
+++ src/pages/RequestAccessPage/RequestAccessPage.vue
@@ -0,0 +1,361 @@
+<template>
+    <SignUpWrapper :sign-up-title="confirm ? $t('register.title-confirm') : $t('register.title')">
+        <div class="confirm-message" v-if="confirm">
+            <i18n-t keypath="register.confirmation" tag="p">
+                <strong>{{ form.email }}</strong>
+            </i18n-t>
+        </div>
+        <template v-else>
+            <div class="form-group">
+                <TextInput
+                    v-model="form.given_name"
+                    :label="$t('register.given_name.label')"
+                    :placeholder="$t('register.given_name.placeholder')"
+                    @blur="v$.form.given_name.$validate"
+                    data-test="first-name"
+                />
+                <p
+                    v-for="error of v$.form.given_name.$errors"
+                    :key="error.$uid"
+                    class="error-message"
+                >
+                    {{ error.$message }}
+                </p>
+            </div>
+            <div class="form-group">
+                <TextInput
+                    v-model="form.family_name"
+                    :label="$t('register.family_name.label')"
+                    :placeholder="$t('register.family_name.placeholder')"
+                    @blur="v$.form.family_name.$validate"
+                    data-test="last-name"
+                />
+                <p
+                    v-for="error of v$.form.family_name.$errors"
+                    :key="error.$uid"
+                    class="error-message"
+                >
+                    {{ error.$message }}
+                </p>
+            </div>
+            <div class="form-group">
+                <TextInput
+                    v-model="form.email"
+                    :label="$t('register.email.label')"
+                    input-type="email"
+                    :placeholder="$t('register.email.placeholder')"
+                    @blur="v$.form.email.$validate"
+                    data-test="email"
+                />
+                <p v-for="error of v$.form.email.$errors" :key="error.$uid" class="error-message">
+                    {{ error.$message }}
+                </p>
+            </div>
+            <div class="form-group">
+                <TextInput
+                    input-type="password"
+                    v-model="form.password"
+                    :label="$t('register.password.label')"
+                    :placeholder="$t('register.password.placeholder')"
+                    @blur="v$.form.password.$validate"
+                    data-test="password"
+                />
+                <p
+                    v-for="error of v$.form.password.$errors"
+                    :key="error.$uid"
+                    class="error-message"
+                >
+                    {{ error.$message }}
+                </p>
+            </div>
+            <div class="action">
+                <LpiButton
+                    :disabled="v$.form.$error || asyncing"
+                    @click="register"
+                    :label="$t('common.confirm')"
+                    :left-icon="asyncing ? 'LoaderSimple' : null"
+                    class="register-btn"
+                    data-test="register-btn"
+                />
+                <i18n-t keypath="register.tos" tag="p" class="tos">
+                    <template #term>
+                        <router-link to="/terms-of-service" class="link"
+                            >{{ $t('register.term') }}
+                        </router-link>
+                    </template>
+
+                    <template #privacy>
+                        <router-link to="/personal-data" class="link"
+                            >{{ $t('register.privacy') }}
+                        </router-link>
+                    </template>
+                </i18n-t>
+            </div>
+        </template>
+        <template #post-box>
+            <div class="extra-links" :class="{ 'is-confirm': confirm }">
+                <p v-if="!confirm" class="extra-link extra-login">
+                    {{ $t('register.have-account') }}
+                    <a href="#" @click.prevent="login" class="link">{{ $t('register.login') }}</a>
+                </p>
+                <p class="extra-link extra-help">
+                    {{ $t('register.need-help') }}
+                    <a href="#" @click.prevent="showContactUsDrawer = true" class="link">{{
+                        $t('register.contact-us')
+                    }}</a>
+                </p>
+            </div>
+        </template>
+    </SignUpWrapper>
+    <DrawerLayout
+        :is-opened="showContactUsDrawer"
+        :has-footer="false"
+        class="medium"
+        confirm-action-name=""
+        :title="$t('footer.contact')"
+        @close="closeDrawer"
+    >
+        <ContactForm v-if="showContactUsDrawer" @close="closeDrawer" />
+    </DrawerLayout>
+</template>
+<script>
+import useVuelidate from '@vuelidate/core'
+import utils from '@/functs/functions.ts'
+import TextInput from '@/components/lpikit/TextInput/TextInput.vue'
+import LpiButton from '@/components/lpikit/LpiButton/LpiButton.vue'
+import ContactForm from '@/components/Drawers/ContactForm.vue'
+import DrawerLayout from '@/components/lpikit/Drawer/DrawerLayout.vue'
+import SignUpWrapper from '@/components/lpikit/SignUpWrapper/SignUpWrapper.vue'
+import { helpers, required, email } from '@vuelidate/validators'
+export default {
+    name: 'RequestAccessPage',
+
+    components: {
+        TextInput,
+        LpiButton,
+        ContactForm,
+        DrawerLayout,
+        SignUpWrapper,
+    },
+
+    data() {
+        return {
+            form: {
+                email: '',
+                given_name: '',
+                family_name: '',
+                password: '',
+                profile_picture: '',
+            },
+            asyncing: false,
+            confirm: false,
+            v$: useVuelidate(),
+            showContactUsDrawer: false,
+        }
+    },
+
+    validations() {
+        return {
+            form: {
+                email: {
+                    required: helpers.withMessage(this.$t('register.email.is-required'), required),
+                    email: helpers.withMessage(this.$t('register.email.is-invalid'), email),
+                },
+                given_name: {
+                    required: helpers.withMessage(
+                        this.$t('register.given_name.is-required'),
+                        required
+                    ),
+                },
+                family_name: {
+                    required: helpers.withMessage(
+                        this.$t('register.family_name.is-required'),
+                        required
+                    ),
+                },
+                password: {
+                    required: helpers.withMessage(
+                        this.$t('register.password.is-required'),
+                        required
+                    ),
+                },
+            },
+        }
+    },
+    async mounted() {
+        this.contactEmail = this.$store.getters['organizations/current']?.contact_email
+    },
+
+    methods: {
+        async requestAccess() {
+            this.v$.form.$validate()
+            if (this.v$.form.$error) {
+                return
+            }
+            this.asyncing = true
+            try {
+                await new Promise((resolve) => setTimeout(resolve, 1000))
+
+                this.confirm = true
+            } catch (error) {
+                if (error?.response?.status === 409) {
+                    this.$store.dispatch('notifications/pushToast', {
+                        message: `${this.$t('register.email-already-exists')}`, // TODO
+                        type: 'error',
+                    })
+                } else {
+                    this.$store.dispatch('notifications/pushToast', {
+                        message: `${this.$t('register.save-error')} ${
+                            // TODO
+                            error?.response?.data?.error || ''
+                        }`,
+                        type: 'error',
+                    })
+                }
+
+                console.error(error)
+            } finally {
+                this.asyncing = false
+            }
+        },
+
+        login() {
+            goToKeycloakLoginPage()
+        },
+
+        closeDrawer() {
+            this.showContactUsDrawer = false
+        },
+    },
+}
+</script>
+<style lang="scss" scoped>
+.confirm {
+    border-radius: $border-radius-m;
+    padding: $space-xl;
+}
+
+.error-message {
+    color: $salmon-dark;
+    margin-top: $space-xs;
+    font-size: $font-size-s;
+}
+
+.action {
+    display: flex;
+    padding-bottom: pxToRem(24px);
+    flex-direction: column;
+    align-items: center;
+
+    .tos {
+        font-size: pxToRem(12px);
+        line-height: pxToRem(18px);
+        padding-top: pxToRem(12px);
+    }
+
+    .register-btn {
+        width: 100%;
+        text-transform: capitalize;
+    }
+}
+
+.extra-links {
+    margin-top: pxToRem(29px);
+    width: 100%;
+    display: flex;
+    flex-flow: row nowrap;
+    gap: $space-unit;
+
+    .extra-link {
+        flex-basis: 50%;
+    }
+
+    .extra-login {
+        text-align: left;
+    }
+
+    .extra-help {
+        text-align: right;
+    }
+
+    &.is-confirm {
+        .extra-link {
+            flex-basis: 100%;
+        }
+
+        .extra-help {
+            text-align: center;
+        }
+    }
+}
+
+.link {
+    display: inline-block; // avoid breaking link on new line
+    color: $primary-dark;
+    text-decoration: underline;
+    font-weight: 400;
+
+    &:hover {
+        text-decoration: none;
+    }
+}
+
+.link-expired {
+    width: 90%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 90px auto;
+
+    // padding-top: pxToRem(20px);
+}
+
+.title {
+    font-size: pxToRem(40px);
+    font-weight: 900;
+    color: $primary-dark;
+    line-height: 1.125;
+    margin-bottom: pxToRem(15px);
+}
+
+.expired-text {
+    font-size: pxToRem(40px);
+    line-height: pxToRem(40px);
+    text-align: center;
+    color: $green;
+    font-weight: 700;
+    padding-left: 30px;
+    padding-right: 30px;
+    padding-bottom: 20px;
+    filter: drop-shadow(8px 0 0 #20393d) drop-shadow(-8px 0 0 #20393d) drop-shadow(0 8px 0 #20393d)
+        drop-shadow(0 -8px 0 #20393d);
+}
+
+.illustration {
+    max-width: 500px;
+    height: 500px;
+    display: flex;
+    justify-content: center;
+    align-items: end;
+    background-size: cover;
+    text-align: center;
+    margin: 0 auto;
+}
+
+.extra-informations {
+    // font-size: smaller;
+    text-align: center;
+    line-height: pxToRem(30px);
+    padding-bottom: 40px;
+
+    a {
+        font-weight: 700;
+        color: $primary-dark;
+        text-decoration: underline;
+    }
+}
+.confirm-message strong {
+    font-weight: 700;
+    color: $primary-dark;
+}
+</style>
