0f18603e4a1512563a932a8b79fed9312faedd7f
Stanislas Bruhière <stanislas@bruhiere.fr>
2024-03-08 16:07:01 +0100

    feat: run as CI, rework pw options

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,11 +28,11 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 1 : 1,
+    retries: 1,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : 1,
+    workers: 1,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+    reporter: process.env.CI ? 'line' : 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
