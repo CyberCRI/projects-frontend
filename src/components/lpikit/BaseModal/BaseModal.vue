28fe1c6dd5558eb02a917980e31ec9eab58528ce
JulienDroulez <julien.droulez@cri-paris.org>
2024-04-02 16:11:02 +0200

    fix: unselected location type in location form modal (#276)

--- src/components/lpikit/BaseModal/BaseModal.vue
+++ src/components/lpikit/BaseModal/BaseModal.vue
@@ -2,7 +2,7 @@
     <div>
         <teleport to="body">
             <transition name="modal-fade">
-                <div class="modal-backdrop" data-test="modal-backdrop" v-show="open">
+                <div class="modal-backdrop" data-test="modal-backdrop" v-if="open">
                     <div class="modal-wrapper" :class="{ 'full-screen': fullScreen }">
                         <div v-if="$slots.topping" class="modal-topping">
                             <slot name="topping"></slot>
