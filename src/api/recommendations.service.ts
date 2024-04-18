0456eaf0f852569db423f96a66509a5c2b2db776
Julien Droulez <julien.droulez@cri-paris.org>
2024-04-18 17:32:25 +0200

    fix: use new endpoits for reco

--- src/api/recommendations.service.ts
+++ src/api/recommendations.service.ts
@@ -9,25 +9,25 @@ export interface RecommendationsParams {
     params?: APIParams
 }
 
-export async function getProjectsRecommendations(
+export async function getProjectsRecommendationsForUser(
     body: RecommendationsParams
 ): Promise<ProjectOutput[]> {
     return (
         await axios.get(
             `${import.meta.env.VITE_APP_API_DEFAULT_VERSION}/organization/${
                 body.organization
-            }/recommended-project/`,
+            }/recommended-project/user/`,
             _adaptParamsToGetQuery(body.params)
         )
     ).data
 }
 
-export async function getUsersRecommendations(body): Promise<UserModel[]> {
+export async function getUsersRecommendationsForUser(body): Promise<UserModel[]> {
     return (
         await axios.get(
             `${import.meta.env.VITE_APP_API_DEFAULT_VERSION}/organization/${
                 body.organization
-            }/recommended-user/`,
+            }/recommended-user/user/`,
             _adaptParamsToGetQuery(body.params)
         )
     ).data
