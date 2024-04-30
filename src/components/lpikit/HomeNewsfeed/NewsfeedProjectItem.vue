762e0138282f2eb80bfb3cb8b99501190dd14998
Julien Droulez <julien.droulez@cri-paris.org>
2024-04-30 11:58:36 +0200

    fix: swap project and announcement card layouts

--- src/components/lpikit/HomeNewsfeed/NewsfeedProjectItem.vue
+++ src/components/lpikit/HomeNewsfeed/NewsfeedProjectItem.vue
@@ -3,8 +3,6 @@
         :to="{ name: 'pageProject', params: { slugOrId: project.slug } }"
         class="home-project-item shadow-box"
     >
-        <h3 class="project-title-mobile">{{ $filters.capitalize(project.title) }}</h3>
-
         <div class="project-img-container">
             <CroppedImage
                 :alt="`${project.title} image`"
@@ -71,32 +69,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$dimension: 120px;
+
 .home-project-item {
     display: flex;
-    flex-direction: column;
     border: $border-width-s solid $gray-10;
     border-radius: $border-radius-s;
     padding: $space-l;
     gap: $space-l;
-
-    @media screen and (min-width: $min-tablet) {
-        flex-direction: row;
-        min-height: 200px;
-        align-items: stretch;
-    }
+    flex-direction: row;
+    min-height: $dimension;
+    align-items: stretch;
 
     .project-img-container {
-        align-self: center;
+        align-self: flex-start;
         background-size: cover;
         background-position: top center;
-        height: pxToRem(200px);
+        height: $dimension;
         flex: none;
         width: 100%;
-
-        @media screen and (min-width: $min-tablet) {
-            margin-bottom: 0;
-            flex-basis: 200px;
-        }
+        margin-bottom: 0;
+        flex-basis: $dimension;
 
         .project-img {
             object-fit: cover;
@@ -108,32 +101,13 @@ export default {
             border: 1px solid $gray-10;
         }
     }
-
-    .project-title-mobile {
-        font-size: $font-size-l;
-        line-height: $font-size-3xl;
-        margin-bottom: $space-m;
-
-        @media screen and (min-width: $min-tablet) {
-            display: none;
-        }
-    }
 }
 
 .project-content {
-    display: flex;
     flex-flow: column nowrap;
-
-    .project-title-desktop {
-        display: none;
-
-        @media screen and (min-width: $min-tablet) {
-            display: block;
-            font-size: $font-size-2xl;
-            line-height: $font-size-3xl;
-            margin-bottom: $space-m;
-        }
-    }
+    display: block;
+    line-height: 1.5;
+    margin-bottom: $space-m;
 
     .project-description {
         flex-grow: 1;
@@ -145,35 +119,6 @@ export default {
         -webkit-line-clamp: 4;
         -webkit-box-orient: vertical;
         overflow: hidden;
-    }
-
-    .more-info {
-        display: flex;
-        align-items: center;
-        margin-top: $space-l;
-        cursor: pointer;
-
-        .arrow {
-            fill: $primary-dark;
-            height: $font-size-2xl;
-            width: $font-size-2xl;
-        }
-
-        .read {
-            color: $primary-dark;
-            font-weight: 700;
-            margin-left: $space-xs;
-        }
-
-        &:hover {
-            .read {
-                transform: scaleX(1.1);
-            }
-
-            .arrow {
-                transform: scale(1.35);
-            }
-        }
     }
 }
 </style>
