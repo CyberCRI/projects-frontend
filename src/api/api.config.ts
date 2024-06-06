a295bfcf6b205049c199a7cb06df312abfcf3ddb
JulienDroulez <julien.droulez@cri-paris.org>
2024-06-06 15:31:51 +0200

    feat: use backend error messages (#401)

--- src/api/api.config.ts
+++ src/api/api.config.ts
@@ -52,40 +52,62 @@ const responseError = (error: AxiosError) => {
 
     // We could add specific notification to display the errors
     // And we could also handle refresh token there if needed when catching error
-    if (error.response && error.response.status === 400) {
-        // Show first message
-        const firstKey = Object.keys(error.response.data)[0]
-        const firstValue = error.response.data[firstKey][0]
-        const message = `${firstKey}: ${firstValue}`
+    if (error.response) {
+        const { data, status }: { data: any; status: number } = error.response
+        if (data.type && (data.errors || data.detail)) {
+            if (data.type === 'validation') {
+                let message = ''
+                for (const key in data.errors) {
+                    message += `${key}: ${data.errors[key].join(' ')}`
+                }
 
-        store.dispatch('notifications/pushToast', {
-            message: message,
-            type: 'warning',
-        })
-    } else if (error.response && error.response.status === 401) {
-        store.dispatch('notifications/pushToast', {
-            //message: i18n.messages[i18n.locale].message['error']['unauthorized'],
-            message: i18n.global.t('message.error.unauthorized'),
-            type: 'error',
-        })
-    } else if (error.response && error.response.status === 422) {
-        store.dispatch('notifications/pushToast', {
-            // message: i18n.messages[i18n.locale].message['error']['unprocessable-entity'],
-            message: i18n.global.t('message.error.unprocessable-entity'),
-            type: 'error',
-        })
-    } else if (error.response && error.response.status === 502) {
-        store.dispatch('notifications/pushToast', {
-            // message: i18n.messages[i18n.locale].message['error']['bad-gateway'],
-            message: i18n.global.t('message.error.bad-gateway'),
-            type: 'error',
-        })
-    }
+                store.dispatch('notifications/pushToast', {
+                    message: message,
+                    type: 'error',
+                })
+            } else {
+                store.dispatch('notifications/pushToast', {
+                    message: data.detail,
+                    type: 'error',
+                })
+            }
+            // other error cases are kept for backward compatibility
+        } else if (status === 400) {
+            // Show first message
+            const firstKey = Object.keys(data)[0]
+            const firstValue = data[firstKey][0]
+            const message = `${firstKey}: ${firstValue}`
 
-    if (error.response && error.response.status === 403 && !originalRequest._retry) {
-        originalRequest._retry = true
+            store.dispatch('notifications/pushToast', {
+                message: message,
+                type: 'warning',
+            })
+        } else if (status === 401) {
+            store.dispatch('notifications/pushToast', {
+                //message: i18n.messages[i18n.locale].message['error']['unauthorized'],
+                message: i18n.global.t('message.error.unauthorized'),
+                type: 'error',
+            })
+        } else if (status === 422) {
+            store.dispatch('notifications/pushToast', {
+                // message: i18n.messages[i18n.locale].message['error']['unprocessable-entity'],
+                message: i18n.global.t('message.error.unprocessable-entity'),
+                type: 'error',
+            })
+        } else if (status === 502) {
+            store.dispatch('notifications/pushToast', {
+                // message: i18n.messages[i18n.locale].message['error']['bad-gateway'],
+                message: i18n.global.t('message.error.bad-gateway'),
+                type: 'error',
+            })
+        }
 
-        return Promise.reject(error)
+        // TODO : is this still used ?
+        if (status === 403 && !originalRequest._retry) {
+            originalRequest._retry = true
+
+            return Promise.reject(error)
+        }
     }
 
     return Promise.reject(error)
