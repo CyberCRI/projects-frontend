f25294a8f58d863e8c9c7213889f67f9cd9010db
Julien Droulez <julien.droulez@cri-paris.org>
2024-06-17 16:59:13 +0200

    fix: hide category dropdown on home if there's no category

--- src/components/category/ProjectCategoriesDropdown.vue
+++ src/components/category/ProjectCategoriesDropdown.vue
@@ -1,12 +1,28 @@
 <template>
-    <div class="project-categories-dropdown" v-click-outside="close" :class="{ 'is-open': open }">
+    <div
+        class="project-categories-dropdown"
+        v-click-outside="close"
+        :class="{ 'is-open': open }"
+        v-if="categories?.length"
+    >
         <button class="categories" @click="toggle_categories">
             <span class="categories-btn">{{ $t('project.categories') }}</span>
             <IconImage class="caret" :name="open ? 'ChevronUp' : 'ChevronDown'" />
         </button>
         <transition name="slide">
             <div v-if="open" class="choose-project-categories">
-                <DropdownCategories />
+                <div class="drop-down-menu custom-scrollbar">
+                    <ul>
+                        <li v-for="item in categories" :key="item.id" class="drop-down-menu-item">
+                            <router-link
+                                class="drop-down-menu-item-content"
+                                :to="{ name: 'Category', params: { id: item.id } }"
+                            >
+                                <span class="label">{{ $filters.capitalize(item.name) }}</span>
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </transition>
     </div>
@@ -14,7 +30,6 @@
 
 <script>
 import IconImage from '@/components/base/media/IconImage.vue'
-import DropdownCategories from '@/components/category/DropdownCategories.vue'
 
 export default {
     name: 'ProjectCategoriesDropdown',
@@ -23,7 +38,6 @@ export default {
 
     components: {
         IconImage,
-        DropdownCategories,
     },
 
     data() {
@@ -31,7 +45,11 @@ export default {
             open: false,
         }
     },
-
+    computed: {
+        categories() {
+            return this.$store.getters['projectCategories/allOrderedByOrderId']
+        },
+    },
     methods: {
         close() {
             this.open = false
@@ -118,6 +136,52 @@ export default {
 
     :deep(a) {
         opacity: 0;
+    }
+}
+
+.drop-down-menu {
+    background-color: $white;
+    border-width: 0 1px 1px;
+    border-style: solid;
+    border-color: $lighter-gray;
+    border-bottom-left-radius: $border-radius-xs;
+    border-bottom-right-radius: $border-radius-xs;
+    max-height: pxToRem(450px);
+    padding-block: $border-radius-s;
+    transition: 0.15s all ease-in-out;
+    cursor: pointer;
+    will-change: transform;
+    overflow-y: scroll;
+
+    .drop-down-menu-item {
+        padding: 0 $space-l;
+        position: relative;
+
+        .drop-down-menu-item-content {
+            color: $primary-dark;
+            padding: $space-m 0;
+            font-size: $font-size-s;
+            display: flex;
+            align-items: center;
+            font-family: Ubuntu, 'Noto Sans SC', helvetica, arial, sans-serif;
+
+            .label {
+                transform-origin: left center;
+                transition: transform 200ms ease-in-out;
+                font-weight: 400;
+                font-size: $font-size-m;
+            }
+        }
+
+        &:hover {
+            background-color: $primary-light;
+
+            .drop-down-menu-item-content {
+                .label {
+                    transform: scaleX(1.1);
+                }
+            }
+        }
     }
 }
 </style>
