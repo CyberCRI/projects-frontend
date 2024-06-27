5fa7e0b4040827e548c0641945789b89abf66c91
Julien Droulez <julien.droulez@cri-paris.org>
2024-06-27 16:49:15 +0200

    fix: cleanup groups page and fix styles

--- src/pages/GroupsPage/GroupsPage.vue
+++ src/pages/GroupsPage/GroupsPage.vue
@@ -2,30 +2,15 @@
     <div class="page-section-extra-wide groups-layout page-top">
         <h1 class="page-title">{{ $filters.capitalize($t('common.groups')) }}</h1>
 
-        <div class="search-groups-ctn" v-if="!groupId && searchOptionsInitiated">
-            <div class="search-input-container">
-                <SearchOptions
-                    :limit="30"
-                    ref="searchOptions"
-                    :search="search"
-                    section="groups"
-                    show-filters
-                    @search-options-updated="updateSearch"
-                />
-                <!--SearchInput
-                    v-model="searchFilter.search"
-                    :full="true"
-                    :placeholder="$t('browse.placeholder')"
-                    class="search-input"
-                    @enter="updateSearchQuery(true)"
-                    @delete-query="searchFilter.search = ''"
-                />
-                <LpiButton
-                    :label="$t('browse.page-title')"
-                    :secondary="false"
-                    @click="updateSearchQuery(true)"
-                /-->
-            </div>
+        <div v-if="!groupId && searchOptionsInitiated" class="search-input-container">
+            <SearchOptions
+                :limit="30"
+                ref="searchOptions"
+                :search="search"
+                section="groups"
+                show-filters
+                @search-options-updated="updateSearch"
+            />
         </div>
         <div class="page-section-wide" v-if="hasSearch">
             <GlobalSearchTab :search="search" />
@@ -273,36 +258,6 @@ export default {
         margin: $space-2xl 0;
     }
 
-    .search-groups-ctn {
-        display: flex;
-        flex-direction: column;
-
-        .search-input-container {
-            display: flex;
-            padding: $space-l;
-            margin: $space-xl 0;
-            background: $primary-lighter;
-            align-items: center;
-            border-radius: $border-radius-17;
-            flex-direction: column;
-
-            .search-input {
-                margin-bottom: pxToRem(16px);
-            }
-
-            @media screen and (min-width: $min-tablet) {
-                padding: pxToRem(32px) pxToRem(84px);
-                border-radius: 17px;
-                flex-direction: row;
-
-                .search-input {
-                    margin-right: $space-l;
-                    margin-bottom: 0;
-                }
-            }
-        }
-    }
-
     .title-ctn {
         display: inline-flex;
         flex-direction: column;
@@ -318,6 +273,36 @@ export default {
         padding: 0;
         width: 100%;
         flex-shrink: 1.5;
+    }
+
+    .search-input-container {
+        display: flex;
+        padding: $space-l;
+        background: $primary-lighter;
+        align-items: center;
+        border-radius: $border-radius-17;
+        flex-direction: column;
+        margin: $space-xl 0;
+
+        .search-input {
+            margin-bottom: pxToRem(16px);
+        }
+
+        @media screen and (min-width: $min-tablet) {
+            padding: pxToRem(32px) pxToRem(84px);
+            border-radius: 17px;
+            flex-direction: row;
+
+            .search-input {
+                margin-right: $space-l;
+                margin-bottom: 0;
+            }
+        }
+    }
+
+    :deep(.search-input-ctn),
+    :deep(.search-block) {
+        flex-grow: 1;
     }
 }
 
