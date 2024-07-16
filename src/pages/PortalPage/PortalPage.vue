2e077911f315fa1d801d6dd01eb38c8fbb2eb80e
Julien Droulez <julien.droulez@cri-paris.org>
2024-07-16 15:39:14 +0200

    fix: rework portal page cards

--- src/pages/PortalPage/PortalPage.vue
+++ src/pages/PortalPage/PortalPage.vue
@@ -11,25 +11,18 @@
                 :key="organisation.id"
                 class="org-card shadow-box"
                 :href="organisation.website_url"
+                target="_blank"
+                :style="{
+                    backgroundColor: organisation.background_color,
+                }"
+                :title="organisation.name"
             >
                 <div
-                    v-if="organisation.logo_image"
-                    class="org-image-container"
-                    :style="{ backgroundColor: organisation.background_color }"
-                >
-                    <img
-                        :alt="organisation.name"
-                        :src="organisation.logo_image.variations.medium"
-                        class="org-image"
-                    />
-                </div>
-
-                <div class="org-content">
-                    <h2 class="org-title">{{ organisation.name }}</h2>
-                    <p v-if="organisation.description" class="org-description">
-                        {{ organisation.description }}
-                    </p>
-                </div>
+                    class="portal-image"
+                    :style="{
+                        backgroundImage: `url(${organisation.logo_image.variations.medium})`,
+                    }"
+                ></div>
             </a>
         </div>
     </div>
@@ -53,73 +46,36 @@ export default {
 </script>
 <style scoped lang="scss">
 .portal-layout {
-    $item-width: pxToRem(62px);
-    $item-height: pxToRem(42px);
-    $item-gap: pxToRem(3px);
-
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-    flex-direction: column;
     margin-bottom: $space-2xl;
-
-    .sub-title {
-        font-size: $font-size-m;
-        font-style: normal;
-        font-weight: 400;
-    }
 
     .org-card-ctn {
         display: flex;
         flex-flow: column wrap;
+        gap: $space-2xl;
+        margin-top: $space-xl;
+        margin-bottom: $space-xl;
 
         .org-card {
             display: flex;
             flex-direction: column;
-            justify-content: center;
-            border-radius: 12px;
+            justify-content: stretch;
+            align-items: stretch;
+            border-radius: $border-radius-m;
             border: $border-width-s solid $primary;
             overflow: hidden;
-            margin: $space-l;
+            padding: $space-m;
+            height: pxToRem(96px);
             width: pxToRem(290px);
 
-            // put back pxToRem(250px); for height when content is displayed again (see .org-content)
-            height: pxToRem(96px);
-
-            .org-image-container {
-                // re-enable when content is displayed again (see .org-content)
-                // min-height: pxToRem(100px);
-                width: inherit;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                border-bottom: $border-width-s solid $primary;
-
-                .org-image {
-                    width: auto;
-                    height: auto;
-                    max-width: 100%;
-                    max-height: 100%;
-                }
-            }
-
-            .org-content {
-                padding: pxToRem(20px);
-                height: 100%;
-                display: none; // hide for now as data is too poor yet
-
-                .org-title {
-                    font-size: $font-size-2xl;
-                    font-style: normal;
-                    font-weight: 700;
-                    line-height: $line-height-tight;
-                }
-
-                .org-description {
-                    font-size: $font-size-m;
-                    font-style: normal;
-                    font-weight: 400;
-                }
+            .portal-image {
+                flex-grow: 1;
+                background-size: contain;
+                background-repeat: no-repeat;
+                background-position: center;
             }
         }
     }
@@ -130,7 +86,6 @@ export default {
         .org-card-ctn {
             flex-direction: row;
             justify-content: center;
-            gap: 20px;
             margin-top: $space-3xl;
             margin-bottom: $space-3xl;
 
