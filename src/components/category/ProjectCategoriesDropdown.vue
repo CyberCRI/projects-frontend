704924c081ccd266cdcc67ddce249f9f287e29f8
Julien Droulez <julien.droulez@cri-paris.org>
2024-07-23 16:34:00 +0200

    feat: use category hierarchy on home

--- src/components/category/ProjectCategoriesDropdown.vue
+++ src/components/category/ProjectCategoriesDropdown.vue
@@ -13,14 +13,11 @@
             <div v-if="open" class="choose-project-categories">
                 <div class="drop-down-menu custom-scrollbar">
                     <ul>
-                        <li v-for="item in categories" :key="item.id" class="drop-down-menu-item">
-                            <router-link
-                                class="drop-down-menu-item-content"
-                                :to="{ name: 'Category', params: { id: item.id } }"
-                            >
-                                <span class="label">{{ $filters.capitalize(item.name) }}</span>
-                            </router-link>
-                        </li>
+                        <ProjectCategoriesDropdownElement
+                            v-for="category in categories"
+                            :key="category.id"
+                            :category="category"
+                        />
                     </ul>
                 </div>
             </div>
@@ -30,7 +27,7 @@
 
 <script>
 import IconImage from '@/components/base/media/IconImage.vue'
-
+import ProjectCategoriesDropdownElement from '@/components/category/ProjectCategoriesDropdownElement.vue'
 export default {
     name: 'ProjectCategoriesDropdown',
 
@@ -38,6 +35,7 @@ export default {
 
     components: {
         IconImage,
+        ProjectCategoriesDropdownElement,
     },
 
     data() {
@@ -47,7 +45,7 @@ export default {
     },
     computed: {
         categories() {
-            return this.$store.getters['projectCategories/allOrderedByOrderId']
+            return this.$store.getters['projectCategories/hierarchy']
         },
     },
     methods: {
@@ -147,41 +145,10 @@ export default {
     border-bottom-left-radius: $border-radius-xs;
     border-bottom-right-radius: $border-radius-xs;
     max-height: pxToRem(450px);
-    padding-block: $border-radius-s;
+    padding: $space-s $space-xs;
     transition: 0.15s all ease-in-out;
     cursor: pointer;
     will-change: transform;
     overflow-y: scroll;
-
-    .drop-down-menu-item {
-        padding: 0 $space-l;
-        position: relative;
-
-        .drop-down-menu-item-content {
-            color: $primary-dark;
-            padding: $space-m 0;
-            font-size: $font-size-s;
-            display: flex;
-            align-items: center;
-            font-family: Ubuntu, 'Noto Sans SC', helvetica, arial, sans-serif;
-
-            .label {
-                transform-origin: left center;
-                transition: transform 200ms ease-in-out;
-                font-weight: 400;
-                font-size: $font-size-m;
-            }
-        }
-
-        &:hover {
-            background-color: $primary-light;
-
-            .drop-down-menu-item-content {
-                .label {
-                    transform: scaleX(1.1);
-                }
-            }
-        }
-    }
 }
 </style>
---
+++ src/components/category/ProjectCategoriesDropdownElement.vue
@@ -0,0 +1,64 @@
+<template>
+    <li class="drop-down-menu-item">
+        <router-link
+            class="drop-down-menu-item-content"
+            :to="{ name: 'Category', params: { id: category.id } }"
+        >
+            <span class="label">{{ $filters.capitalize(category.name) }}</span>
+        </router-link>
+        <ul class="sub-list">
+            <ProjectCategoriesDropdownElement
+                :category="child"
+                v-for="child in category.children"
+                :key="child.id"
+            />
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'ProjectCategoriesDropdownElement',
+
+    props: {
+        category: {
+            type: Object,
+            required: true,
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.drop-down-menu-item {
+    position: relative;
+
+    .drop-down-menu-item-content {
+        color: $primary-dark;
+        padding: $space-m $space-s;
+        font-size: $font-size-s;
+        display: flex;
+        align-items: center;
+        font-family: Ubuntu, 'Noto Sans SC', helvetica, arial, sans-serif;
+
+        .label {
+            transform-origin: left center;
+            transition: transform 200ms ease-in-out;
+            font-weight: 400;
+            font-size: $font-size-m;
+        }
+
+        &:hover {
+            background-color: $primary-light;
+
+            .label {
+                transform: scaleX(1.1);
+            }
+        }
+    }
+}
+
+.sub-list {
+    margin-left: 2ex;
+}
+</style>
