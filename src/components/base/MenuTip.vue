f012e2239cbcc489c53d9620efad963558708991
Julien Droulez <julien.droulez@cri-paris.org>
2024-07-24 17:44:02 +0200

    fix: close menu-tip on action trigger

--- src/components/base/MenuTip.vue
+++ src/components/base/MenuTip.vue
@@ -52,7 +52,7 @@ export default {
             openedMenuTip.value = null
         },
 
-        closeAfterClick(event) {
+        closeAfterClick() {
             this.$nextTick(() => {
                 this.close()
             })
