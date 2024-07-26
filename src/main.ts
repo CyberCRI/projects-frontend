b7d6ab84b78bc0de47d0b6cd223014016a08e043
Stanislas Bruhière <46960549+Nastaliss@users.noreply.github.com>
2024-07-26 11:21:14 +0200

    feat: use sentry (#433)
    * feat: use sentry
    
    * fix: pass env variables, use new runner
    
    * feat: decrease ram usage
    
    * feat: increase ram
    
    * fix: more mem
    
    * feat: lot of memories
    
    * feat: a bit less ram
    
    * feat: string
    
    * feat: add debug
    
    * fix: more debug
    
    * fix: add VITE_APP in dockerfile
    
    * fix: cleanup and pass frontend environment

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ import analytics from '@/analytics'
 import '@/design/scss/reset.scss'
 import '@/design/scss/main.scss'
 import router from '@/router'
+import * as Sentry from '@sentry/vue'
 
 import { goToKeycloakLoginPage } from '@/api/auth/auth.service'
 
@@ -89,6 +90,31 @@ async function main(): Promise<void> {
     //     render: (h) => h(App),
     // })
     const app = createApp(App)
+
+    const SENTRY_ENABLED = import.meta.env.VITE_APP_SENTRY_ENABLED
+    if (SENTRY_ENABLED) {
+        Sentry.init({
+            app,
+            dsn: import.meta.env.VITE_APP_SENTRY_DSN,
+            integrations: [
+                Sentry.browserTracingIntegration({ router }),
+                Sentry.replayIntegration(),
+            ],
+            environment: import.meta.env.VITE_APP_ENVIRONMENT,
+            tracePropagationTargets: ['localhost', import.meta.env.VITE_APP_API_URL],
+
+            // Set tracesSampleRate to 1.0 to capture 100%
+            // of transactions for tracing.
+            // We recommend adjusting this value in production
+            tracesSampleRate: 1.0,
+
+            // Capture Replay for 10% of all sessions,
+            // plus for 100% of sessions with an error
+            replaysSessionSampleRate: 0.1,
+            replaysOnErrorSampleRate: 1.0,
+            release: import.meta.env.VITE_APP_SENTRY_RELEASE,
+        })
+    }
 
     // Display errors / warnings
     app.config.errorHandler = (err, vm, info) => {
--- vite.config.js
+++ vite.config.js
@@ -65,5 +65,14 @@ export default defineConfig(({ command, mode }) => {
                 port: 3010,
             },
         },
+        build: {
+            rollupOptions: {
+                output: {
+                    entryFileNames: 'assets/[name].js',
+                    chunkFileNames: `assets/[name].js`,
+                    assetFileNames: `assets/[name].[ext]`,
+                },
+            },
+        },
     }
 })
