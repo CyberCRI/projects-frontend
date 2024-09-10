06b0f82f63ef6f57ca8849a2ff6249c46fc2e1ad
JulienDroulez <julien.droulez@cri-paris.org>
2024-09-10 17:26:10 +0200

    fix: e2e fail on extra project tabs (#468)

--- tests/playwright/lib/project/destroy.ts
+++ tests/playwright/lib/project/destroy.ts
@@ -1,5 +1,11 @@
 export async function destroyProject(page) {
-    await page.locator('[data-test="project-settings"]').click()
+    let settings = await page.locator('[data-test="project-settings"]')
+    // button might be hidden in extra tabs dropdown
+    if (!settings) {
+        await page.locator('[data-test="extra-tabs-button"]').click()
+        settings = await page.locator('[data-test="project-settings"]')
+    }
+    await settings.click()
     await page.locator('[data-test="destroy-project"]').click()
     await page.locator('[data-test="confirm-destroy"]').click()
 }
