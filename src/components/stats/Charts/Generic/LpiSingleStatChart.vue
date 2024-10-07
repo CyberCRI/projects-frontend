dee313bf10e81ca2df325fae11517aa6f9c8a64d
Stanislas Bruhière <stanislas@bruhiere.fr>
2024-10-07 17:54:08 +0200

    feat: add singlestat

---
+++ src/components/stats/Charts/Generic/LpiSingleStatChart.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="total-container">
+        <span class="projects-count">{{ chartData }}</span> <span class="projects-word">{{ $t('stats.projects') }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LpiSingleStatChart',
+    props: {
+        chartData: {
+            type: Number,
+            default: null,
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.total-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    .projects-count {
+        font-size: 7rem;
+        font-weight: 700;
+        color: $primary-dark;
+        margin-bottom: -10px;
+    }
+
+    .projects-word {
+        font-size: 1.5rem;
+        font-weight: 700;
+        color: $salmon;
+    }
+}
+</style>
