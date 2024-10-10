ea3d8b910feded77664830c4e9d0886f73da6ed3
JulienDroulez <julien.droulez@cri-paris.org>
2024-10-10 11:28:55 +0200

    fix: try to fix logut isuue in e2e (#523)

--- tests/playwright/lib/login/logout.ts
+++ tests/playwright/lib/login/logout.ts
@@ -1,15 +1,17 @@
 import { Page } from '@playwright/test'
 import { LogLevel, Logger } from '../../logger'
-import { delay } from '../index'
+import { expect } from '@playwright/test'
+// import { delay } from '../index'
 
 const logger = new Logger(LogLevel.Debug)
 
 export async function logOut(page: Page) {
-    await page.locator('[data-test="lpi-logo"]').click()
-    await delay(3000)
+    // await page.locator('[data-test="lpi-logo"]').click()
+    // await delay(3000)
     await page.locator('[data-test="dropdown-user-account"]').click()
     logger.info('Click to disconnect')
     await page.locator('[data-test="disconnect"]').click()
-    await delay(1000)
+    // await delay(1000)
+    await expect(page.locator('[data-test="test-login-button"]')).toBeVisible()
     logger.info('Disconnected')
 }
