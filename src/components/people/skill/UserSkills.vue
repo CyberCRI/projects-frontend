11afe9ecd220e94ded9add4740fcdf505ebab749
Julien Droulez <julien.droulez@cri-paris.org>
2024-11-26 16:12:06 +0100

    fix: rework profile skill presentation

--- src/components/people/skill/UserSkills.vue
+++ src/components/people/skill/UserSkills.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
+        <div v-if="title" class="title-wrapper">
+            <h3 class="title">{{ title }}</h3>
+            <SkillLevelTip>
+                <LinkButton
+                    :label="$t(`me.levels-help-link`)"
+                    btn-icon="HelpCircle"
+                    data-test="skill-levels-help-button"
+                />
+            </SkillLevelTip>
+        </div>
         <div class="columns-wrapper" :class="`layout-${columnCount.length}-columns`">
             <div
                 v-for="column in columnCount"
@@ -7,11 +17,13 @@
                 class="column"
                 :class="[`column-${column}`]"
             >
-                <h3 v-if="title && column === 0" class="title">{{ title }}</h3>
-
                 <template v-for="(skill, idx) in visibleSkills">
                     <SkillItem
-                        v-if="(idx + idxOffset) % columnCount.length == column"
+                        v-if="
+                            column != 'empty' &&
+                            skillsPerColumn[column].from <= idx &&
+                            idx < skillsPerColumn[column].to
+                        "
                         :key="skill.id"
                         :label="skillTexts.title(skill)"
                         :description="skillTexts.description(skill)"
@@ -26,11 +38,13 @@
 <script>
 import SkillItem from '@/components/people/skill/SkillItem.vue'
 import useSkillTexts from '@/composables/useSkillTexts.js'
+import SkillLevelTip from '@/components/people/skill/SkillLevelTip.vue'
+import LinkButton from '@/components/base/button/LinkButton.vue'
 
 export default {
     name: 'UserSkills',
 
-    components: { SkillItem },
+    components: { SkillItem, LinkButton, SkillLevelTip },
 
     setup() {
         const skillTexts = useSkillTexts()
@@ -82,10 +96,18 @@ export default {
         visibleSkills() {
             return this.fullList ? this.skills : this.skills.slice(0, this.limit)
         },
-        idxOffset() {
-            // if we have a title, it take the place of the first skill
-            // so this is used to offset skill indexes in computations
-            return this.title ? 1 : 0
+        skillsPerColumn() {
+            const res = []
+            let remaining = this.visibleSkills.length
+            let from = 0
+            for (let i = 0; i < this.columnCount.length; i++) {
+                let count = Math.ceil(remaining / (this.columnCount.length - res.length))
+                let to = from + count
+                res.push({ from, to })
+                from += count
+                remaining -= count
+            }
+            return res
         },
     },
 
@@ -103,12 +125,8 @@ export default {
             }
 
             // if there's more column than skills, delete extraneous
-            if (this.columnCount.length > this.skills.length + this.idxOffset) {
-                for (
-                    let i = this.skills.length + this.idxOffset;
-                    i < this.columnCount.length;
-                    i++
-                ) {
+            if (this.columnCount.length > this.skills.length) {
+                for (let i = this.skills.length; i < this.columnCount.length; i++) {
                     // hack column class name to hide it
                     this.columnCount[i] = 'empty'
                 }
@@ -119,10 +137,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$column-spacing: pxToRem(32px);
+
+.title-wrapper {
+    padding-left: $column-spacing;
+    margin-top: 1rem;
+    display: flex;
+    justify-content: space-between;
+    gap: 1rem;
+}
+
+.title {
+    color: $primary-dark;
+    font-size: $font-size-l;
+    text-transform: uppercase;
+}
+
 .columns-wrapper {
     margin-top: $space-2xl;
     display: flex;
-    gap: pxToRem(31px);
+    gap: $column-spacing;
+}
+
+.title-wrapper + .columns-wrapper {
+    margin-top: $space-l;
 }
 
 .layout-1-columns .column {
@@ -142,19 +180,13 @@ export default {
     flex-flow: column;
     gap: $space-m;
     background-color: $primary-lighter;
-    padding: pxToRem(24px) pxToRem(32px);
+    padding: pxToRem(24px) $column-spacing;
     border-radius: $border-radius-l;
     align-items: stretch;
 }
 
 .column-empty {
     opacity: 0;
-}
-
-.title {
-    color: $primary-dark;
-    font-size: $font-size-l;
-    text-transform: uppercase;
 }
 
 .s-button {
