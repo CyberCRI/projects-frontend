6b187946e7d2fd3a6574c0816bd506cc3c8dbfb4
Julien Droulez <julien.droulez@cri-paris.org>
2024-12-11 15:51:33 +0100

    fix: use new endpoints for report and contact forms

--- src/api/report.service.ts
+++ src/api/report.service.ts
@@ -2,18 +2,33 @@ import { ReportCreateModel, ContactModel } from '@/models/report.model'
 import { AxiosResponse } from 'axios'
 import { axios } from '@/api/api.config'
 
-export async function reportBug(formData: ReportCreateModel): Promise<AxiosResponse> {
+export async function reportBug(
+    orgCode: String,
+    formData: ReportCreateModel
+): Promise<AxiosResponse> {
     return (
-        await axios.post(`${import.meta.env.VITE_APP_API_DEFAULT_VERSION}/report/bug/`, formData)
+        await axios.post(
+            `${import.meta.env.VITE_APP_API_DEFAULT_VERSION}/organization/${orgCode}/report/bug/`,
+            formData
+        )
     ).data
 }
 
-export async function reportAbuse(formData: ReportCreateModel): Promise<AxiosResponse> {
+export async function reportAbuse(
+    orgCode: String,
+    formData: ReportCreateModel
+): Promise<AxiosResponse> {
     return (
-        await axios.post(`${import.meta.env.VITE_APP_API_DEFAULT_VERSION}/report/abuse/`, formData)
+        await axios.post(
+            `${import.meta.env.VITE_APP_API_DEFAULT_VERSION}/organization/${orgCode}/report/abuse/`,
+            formData
+        )
     ).data
 }
 
-export async function contactUs(formData: ContactModel): Promise<AxiosResponse> {
-    return await axios.post(`${import.meta.env.VITE_APP_API_DEFAULT_VERSION}/contact/us/`, formData)
+export async function contactUs(orgCode: String, formData: ContactModel): Promise<AxiosResponse> {
+    return await axios.post(
+        `${import.meta.env.VITE_APP_API_DEFAULT_VERSION}/organization/${orgCode}/contact/us/`,
+        formData
+    )
 }
