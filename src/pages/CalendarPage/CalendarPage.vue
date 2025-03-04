6c707aad9930f317e2f0d8e708b7b0424465812f
Julien Droulez <julien.droulez@cri-paris.org>
2025-03-04 14:31:26 +0100

    feat: calendarpage ssr

--- src/pages/CalendarPage/CalendarPage.vue
+++ src/pages/CalendarPage/CalendarPage.vue
@@ -1,3 +1,43 @@
+<script setup>
+import { getOrganizationByCode } from '@/api/organizations.service'
+
+const { canCreateEvent } = usePermissions()
+const { t } = useI18n()
+const router = useRouter()
+const tabs = computed(() => {
+    return [
+        {
+            key: 'future-events',
+            label: t('event.calendar.future'),
+            view: { name: 'FutureEvents' },
+        },
+        {
+            key: 'past-events',
+            label: t('event.calendar.past'),
+            view: { name: 'PastEvents' },
+        },
+    ]
+})
+
+const createEvent = () => {
+    router.push({ name: 'CreateEvent' })
+}
+
+try {
+    const runtimeConfig = useRuntimeConfig()
+    const organization = await getOrganizationByCode(runtimeConfig.public.appApiOrgCode)
+
+    useLpiHead(
+        useRequestURL().toString(),
+        t('event.calendar.title'),
+        organization?.dashboard_subtitle,
+        organization?.banner_image?.variations?.medium
+    )
+} catch (err) {
+    // DGAF
+    console.log(err)
+}
+</script>
 <template>
     <div class="page-section-narrow page-top">
         <h1 class="page-title">{{ $t('event.calendar.title') }}</h1>
@@ -16,44 +56,6 @@
         <TabsLayout :tabs="tabs" :border="false" align-left router-view />
     </div>
 </template>
-<script>
-import TabsLayout from '@/components/base/navigation/TabsLayout.vue'
-import LpiButton from '@/components/base/button/LpiButton.vue'
-import permissions from '@/mixins/permissions.ts'
-
-export default {
-    name: 'CalendarPage',
-
-    mixins: [permissions],
-
-    components: {
-        TabsLayout,
-        LpiButton,
-    },
-
-    computed: {
-        tabs() {
-            return [
-                {
-                    key: 'future-events',
-                    label: this.$t('event.calendar.future'),
-                    view: { name: 'FutureEvents' },
-                },
-                {
-                    key: 'past-events',
-                    label: this.$t('event.calendar.past'),
-                    view: { name: 'PastEvents' },
-                },
-            ]
-        },
-    },
-    methods: {
-        createEvent() {
-            this.$router.push({ name: 'CreateEvent' })
-        },
-    },
-}
-</script>
 <style lang="scss" scoped>
 .page-title {
     margin-bottom: pxToRem(60px);
