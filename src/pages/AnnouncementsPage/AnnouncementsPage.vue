cd092015dcfc30c8adeb6c7726cd021184520aa8
Julien Droulez <julien.droulez@cri-paris.org>
2025-03-04 16:42:47 +0100

    feat: annoucement page ssr and script setup

--- src/pages/AnnouncementsPage/AnnouncementsPage.vue
+++ src/pages/AnnouncementsPage/AnnouncementsPage.vue
@@ -1,3 +1,55 @@
+<script setup>
+import { getAnnouncements } from '@/api/announcements.service'
+import useOrganizationsStore from '@/stores/useOrganizations.ts'
+import { getOrganizationByCode } from '@/api/organizations.service'
+
+const organizationsStore = useOrganizationsStore()
+const { t } = useI18n()
+
+const announcements = useState(() => [])
+const isLoading = useState(() => false)
+
+const organization = computed(() => {
+    return organizationsStore.current
+})
+
+const doGetAnnouncements = async () => {
+    try {
+        isLoading.value = true
+        const { results } = await getAnnouncements({
+            organizations: [organization.value.code],
+            ordering: '-updated_at',
+        })
+        announcements.value = results.filter(
+            (announcement) =>
+                announcement.project.publication_status !== 'private' &&
+                (!announcement.deadline ||
+                    new Date(announcement.deadline) >= new Date().setHours(0, 0, 0, 0))
+        )
+    } catch (err) {
+        console.error(err)
+    } finally {
+        isLoading.value = false
+    }
+}
+
+onMounted(doGetAnnouncements)
+
+try {
+    const runtimeConfig = useRuntimeConfig()
+    const organization = await getOrganizationByCode(runtimeConfig.public.appApiOrgCode)
+
+    useLpiHead(
+        useRequestURL().toString(),
+        t('home.announcements'),
+        organization?.dashboard_subtitle,
+        organization?.banner_image?.variations?.medium
+    )
+} catch (err) {
+    // DGAF
+    console.log(err)
+}
+</script>
 <template>
     <div class="announcements-page page-section-medium page-top">
         <h1 class="page-title">{{ $filters.capitalize($t('home.announcements')) }}</h1>
@@ -7,68 +59,6 @@
         <AnnouncementCardList v-else :announcements="announcements" :is-more-toggled="true" />
     </div>
 </template>
-<script>
-import AnnouncementCardList from '@/components/project/announcement/AnnouncementCardList.vue'
-import AnnouncementCardListSkeleton from '@/components/project/announcement/AnnouncementCardListSkeleton.vue'
-import { getAnnouncements } from '@/api/announcements.service'
-import useOrganizationsStore from '@/stores/useOrganizations.ts'
-
-export default {
-    name: 'AnnouncementsPage',
-
-    components: {
-        AnnouncementCardList,
-        AnnouncementCardListSkeleton,
-    },
-
-    setup() {
-        const organizationsStore = useOrganizationsStore()
-        return {
-            organizationsStore,
-        }
-    },
-    data() {
-        return {
-            announcements: [],
-            isLoading: false,
-        }
-    },
-
-    computed: {
-        organization() {
-            return this.organizationsStore.current
-        },
-    },
-
-    mounted() {
-        this.getAnnouncements()
-    },
-
-    methods: {
-        getAnnouncements() {
-            this.isLoading = true
-            getAnnouncements({
-                organizations: [this.organization.code],
-                ordering: '-updated_at',
-            })
-                .then(({ results }) => {
-                    this.announcements = results.filter(
-                        (announcement) =>
-                            announcement.project.publication_status !== 'private' &&
-                            (!announcement.deadline ||
-                                new Date(announcement.deadline) >= new Date().setHours(0, 0, 0, 0))
-                    )
-                })
-                .catch((err) => {
-                    console.error(err)
-                })
-                .finally(() => {
-                    this.isLoading = false
-                })
-        },
-    },
-}
-</script>
 <style lang="scss" scoped>
 .page-title {
     margin-bottom: $space-2xl;
