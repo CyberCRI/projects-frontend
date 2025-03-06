c31c78bf566cb69624bd7286651d1591d343c4d7
Julien Droulez <julien.droulez@cri-paris.org>
2025-03-06 15:22:40 +0100

    fix: temp enable error log in prod

--- src/server/plugins/nitroError.ts
+++ src/server/plugins/nitroError.ts
@@ -1,6 +1,7 @@
 import { defineNitroPlugin } from 'nitropack/dist/runtime/plugin'
 export default defineNitroPlugin((nuxtApp) => {
     nuxtApp.hooks.hook('error', (error) => {
-        if (import.meta.dev) console.error('Nitro:Error', error)
+        // if (import.meta.dev)
+        console.error('Nitro:Error', error)
     })
 })
