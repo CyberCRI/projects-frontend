989263bf1a9ef56542ea5d52ec866231674e55dc
Julien Droulez <julien.droulez@cri-paris.org>
2025-03-07 17:59:46 +0100

    feat: notification settings ssr

--- src/pages/NotificationSettingsPage/NotificationSettingsPage.vue
+++ src/pages/NotificationSettingsPage/NotificationSettingsPage.vue
@@ -1,3 +1,31 @@
+<script setup>
+import NotificationsSettingsTab from './Tabs/NotificationsSettingsTab.vue'
+import { getOrganizationByCode } from '@/api/organizations.service'
+
+const { t } = useI18n()
+
+const tabs = computed(() => [
+    {
+        key: 'notifications-settings',
+        label: t('notifications.tabs.email'),
+        component: NotificationsSettingsTab,
+    },
+])
+
+try {
+    const runtimeConfig = useRuntimeConfig()
+    const organization = await getOrganizationByCode(runtimeConfig.public.appApiOrgCode)
+    useLpiHead(
+        useRequestURL().toString(),
+        computed(() => t('notifications.title')),
+        organization?.dashboard_subtitle,
+        organization?.banner_image?.variations?.medium
+    )
+} catch (err) {
+    console.log(err)
+}
+</script>
+
 <template>
     <div class="settings-layout page-section-extra-wide page-top">
         <h1 class="page-title">{{ $t('notifications.title') }}</h1>
@@ -5,25 +33,3 @@
         <TabsLayout :align-left="true" :border="false" :tabs="tabs"></TabsLayout>
     </div>
 </template>
-
-<script>
-import TabsLayout from '@/components/base/navigation/TabsLayout.vue'
-import NotificationsSettingsTab from './Tabs/NotificationsSettingsTab.vue'
-
-export default {
-    name: 'NotificationSettingsPage',
-    components: { TabsLayout },
-
-    computed: {
-        tabs() {
-            return [
-                {
-                    key: 'notifications-settings',
-                    label: this.$t('notifications.tabs.email'),
-                    component: NotificationsSettingsTab,
-                },
-            ]
-        },
-    },
-}
-</script>
