f4989de0d80433686ee35fed1986ad1a3c1ec27d
Julien Droulez <julien.droulez@learningplanetinstitute.org>
2025-09-16 17:32:55 +0200

    feat: quick edit for project locations

--- src/pages/ProjectPageV2/Tabs/ProjectLocationsTab.vue
+++ src/pages/ProjectPageV2/Tabs/ProjectLocationsTab.vue
@@ -18,7 +18,18 @@
         <h3 class="list-title">Team</h3>
         <ul class="location-list">
           <li v-for="location in teamLocations" :key="location.id" class="location">
-            {{ location.title }}
+            <div class="location-title">
+              <h4>{{ location.title }}</h4>
+              <p>{{ location.description }}</p>
+            </div>
+            <ContextActionMenu
+              v-if="isInEditingMode"
+              class="locatiopn-actions"
+              can-edit
+              can-delete
+              @edit="openEditModal(location)"
+              @delete="locationToDelete = location"
+            />
           </li>
         </ul>
       </div>
@@ -26,18 +37,56 @@
         <h3 class="list-title">Impact</h3>
         <ul class="location-list">
           <li v-for="location in impactLocations" :key="location.id" class="location">
-            {{ location.title }}
+            <div class="location-title">
+              <h4>{{ location.title }}</h4>
+              <p>{{ location.description }}</p>
+            </div>
+            <ContextActionMenu
+              v-if="isInEditingMode"
+              class="location-actions"
+              can-edit
+              can-delete
+              @edit="openEditModal(location)"
+              @delete="locationToDelete = location"
+            />
           </li>
         </ul>
       </div>
     </div>
+
+    <LocationForm
+      v-if="formVisible"
+      :project-id="projectId"
+      :address="newLocationAddress"
+      :location-to-be-edited="locationToBeEdited"
+      :new-coordinates="newCoordinates"
+      @close="closeLocationForm"
+      @center-map="centerMap"
+      @location-edited="$emit('reload-locations')"
+      @location-created="onLocationCreated"
+      @location-deleted="$emit('reload-locations')"
+    />
+
+    <ConfirmModal
+      v-if="locationToDelete"
+      :asyncing="deleteAsyncing"
+      :title="$t('geocoding.delete-location')"
+      :content="$t('geocoding.confirm-delete-location')"
+      @cancel="locationToDelete = null"
+      @confirm="onConfirmDeleteLocation"
+    />
   </div>
 </template>
 <script>
+import useToasterStore from '@/stores/useToaster'
+import analytics from '@/analytics'
+import { deleteLocation } from '@/api/locations.services'
+
 export default {
   name: 'ProjectLocationsTab',
 
   inject: ['projectLayoutToggleAddModal'],
+
   props: {
     locations: {
       type: Array,
@@ -49,10 +98,22 @@ export default {
     },
   },
 
+  emits: ['reload-locations'],
+
   setup() {
     const { canEditProject } = usePermissions()
+    const toaster = useToasterStore()
     return {
       canEditProject,
+      toaster,
+    }
+  },
+  data() {
+    return {
+      formVisible: false,
+      locationToBeEdited: null,
+      locationToDelete: null,
+      deleteAsyncing: false,
     }
   },
 
@@ -62,6 +123,46 @@ export default {
     },
     impactLocations() {
       return this.locations.filter((l) => l.type === 'impact')
+    },
+  },
+
+  methods: {
+    async onConfirmDeleteLocation() {
+      try {
+        this.deleteAsyncing = true
+        await deleteLocation(this.locationToDelete)
+
+        analytics.location.deleteLocationMapPoint({
+          project: {
+            id: this.projectId,
+          },
+          location: this.locationToDelete,
+        })
+
+        this.toaster.pushSuccess(this.$t('toasts.location-delete.success'))
+
+        this.$emit('reload-locations')
+        this.$nextTick(() => this.centerMap())
+      } catch (error) {
+        this.toaster.pushError(`${this.$t('toasts.location-delete.error')} (${error})`)
+        console.error(error)
+      } finally {
+        this.deleteAsyncing = false
+        this.locationToDelete = null
+      }
+    },
+
+    openEditModal(location) {
+      if (this.isInEditingMode) {
+        // restart from source locations because the map ones are not reactive
+        this.locationToBeEdited = location
+        this.formVisible = true
+      }
+    },
+
+    closeLocationForm() {
+      this.locationToBeEdited = null
+      this.formVisible = false
     },
   },
 }
@@ -82,15 +183,45 @@ export default {
 .list-title {
   text-transform: uppercase;
   font-size: $font-size-l;
-  font-weight: 400;
+  color: $primary-dark;
+  font-weight: 700;
 }
 
 .location-list {
+  list-style: none;
+  display: grid;
   margin-top: 1rem;
-  margin-left: 1rem;
-  list-style-type: disc;
-  list-style-position: inside;
-  columns: 3;
-  font-weight: 600;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 1rem;
+
+  @media screen and (max-width: $max-tablet) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media screen and (max-width: $min-tablet) {
+    grid-template-columns: 1fr;
+  }
+
+  li {
+    display: flex;
+    background-color: $primary-lighter;
+    border-radius: 0.8rem;
+    padding: 0.5em;
+    align-items: flex-start;
+    gap: 1rem;
+
+    .location-title {
+      flex-grow: 1;
+
+      h4 {
+        font-weight: 600;
+      }
+
+      p {
+        font-size: 0.8rem;
+        font-weight: 300;
+      }
+    }
+  }
 }
 </style>
