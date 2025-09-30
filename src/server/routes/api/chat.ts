42288f3a5ff9a2e9d6414650441f51eebdbd0da8
Julien Droulez <julien.droulez@learningplanetinstitute.org>
2025-09-30 16:47:33 +0200

    fix: use previous message id

--- src/server/routes/api/chat.ts
+++ src/server/routes/api/chat.ts
@@ -12,7 +12,18 @@ export default defineLazyEventHandler(() => {
   })
 
   return defineEventHandler(async (event) => {
-    const { messages } = await readBody<{ messages }>(event)
+    const { messages, previousResponseId } = await readBody<{
+      messages: Array<{ role: string; text: string }>
+      previousResponseId?: string
+    }>(event)
+
+    // console.log('\x1b[36m%s\x1b[0m', 'MESSAGES FROM FRONTEND')
+    // messages.forEach((message) => {
+    //   console.log('\x1b[36m%s\x1b[0m', JSON.stringify(message, null, 2))
+    // })
+
+    // console.log('\x1b[36m%s\x1b[0m', 'PREVIOUS MESSAGE ID')
+    // console.log('\x1b[36m%s\x1b[0m', previousResponseId)
 
     const adaptedMessages = messages.map((message) => {
       return {
@@ -28,13 +39,31 @@ export default defineLazyEventHandler(() => {
       prompt['version'] = String(appOpenaiApiPromptVersion)
     }
 
-    const response = await openai.responses.create({
+    const requestOptions = {
       prompt,
-      store: false, // do not store in OpenAI's servers, we do this on client side
+      //store: false, // do not store in OpenAI's servers, we do this on client side
+      store: true, // we want to store it to be able to use follow-up questions
       input: adaptedMessages, // [{ role: 'user', content: messages[0].content }]
-    })
+      previous_response_id: previousResponseId || undefined,
+      reasoning: {},
+      tools: [
+        {
+          type: 'file_search',
+          vector_store_ids: ['vs_68da7bdcdba081918133c06e5984489b'],
+        },
+      ],
+    }
+
+    // console.log('\x1b[36m%s\x1b[0m', 'OPENAI REQUEST')
+    // console.log(JSON.stringify(requestOptions, null, 2), 'color:#0ff;')
+
+    const response = await openai.responses.create(requestOptions as any)
+
+    // console.log('\x1b[36m%s\x1b[0m', 'OPENAI RESPONSE')
+    // console.log('OpenAI response:\n%c' + JSON.stringify(response, null, 2), 'color:#ff0;')
 
     const adaptedResponse = {
+      id: response.id,
       text: response.output_text,
     }
 
