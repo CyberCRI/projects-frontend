330c639d3be95aa1b7a3907cb47160ae615653d8
JulienDroulez <julien.droulez@cri-paris.org>
2025-12-19 10:19:42 +0100

    fix: update notification api (#877)

--- src/api/notifications.service.ts
+++ src/api/notifications.service.ts
@@ -1,7 +1,11 @@
 import type { APIParams /*, APIResponseList*/ } from '@/api/types'
 // import type { NotificationModel } from '@/models/notifications.model'
 import useAPI from '@/composables/useAPI'
+import { useRuntimeConfig } from '#imports'
 
-export async function getNotifications(params: APIParams) {
-  return await useAPI(`notification/`, { params }) //.data.value
+export async function getNotifications(params: APIParams, orgCode: string | null = null) {
+  const runtimeConfig = useRuntimeConfig()
+  const currentOrgCode = runtimeConfig.public.appApiOrgCode
+
+  return await useAPI(`organization/${orgCode || currentOrgCode}/notification/`, { params }) //.data.value
 }
