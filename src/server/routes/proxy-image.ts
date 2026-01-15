62957ec69983acacdc6c7ba864fb2f6181344d2c
Julien Droulez <julien.droulez@learningplanetinstitute.org>
2026-01-15 11:28:22 +0100

    fix: allowed image proxy domains

--- src/server/routes/proxy-image.ts
+++ src/server/routes/proxy-image.ts
@@ -1,8 +1,9 @@
 export default defineEventHandler(async (event) => {
   const target = getQuery(event)?.url as string
   if (
+    // TODO: use environment variables to define allowed domains
     !target?.match(
-      /^https:\/\/(criparisprodprodassets|criparisdevlabassets|criparisdevlabprojects).blob.core.windows.net\//
+      /^https:\/\/(criparisprodprodassets|criparisdevlabassets|criparisdevlabprojects|criparisprodprodprojects).blob.core.windows.net\//
     )
   ) {
     // return 404
