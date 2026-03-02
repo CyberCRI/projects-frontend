84dabca5891c8d081485bcf082234023689dc181
rgermain <contact@germainremi.fr>
2026-03-02 12:04:41 +0100

    fix: refresh groups

---
+++ tests/unit/functs/query.test.ts
@@ -0,0 +1,41 @@
+import { withQuery } from '@/functs/query'
+import { describe, expect, it } from 'vitest'
+describe('withQuery', () => {
+  it('empty query', () => {
+    expect(withQuery(null)).toEqual([])
+    expect(withQuery(undefined)).toEqual([])
+    expect(withQuery({})).toEqual([])
+  })
+
+  it('string query', () => {
+    expect(
+      withQuery({
+        search: 'asearch',
+      })
+    ).toEqual([['search', 'asearch']])
+  })
+  it('number query', () => {
+    expect(
+      withQuery({
+        page: 42,
+      })
+    ).toEqual([['page', '42']])
+  })
+  it('boolean query', () => {
+    expect(
+      withQuery({
+        user: true,
+      })
+    ).toEqual([['user', 'true']])
+  })
+  it('array query', () => {
+    expect(
+      withQuery({
+        modules: ['members', 'subgroups'],
+      })
+    ).toEqual([
+      ['modules', 'members'],
+      ['modules', 'subgroups'],
+    ])
+  })
+})
