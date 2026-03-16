cebd0d7f87b121fd10552075da43351622aac2d8
rgermain <contact@germainremi.fr>
2026-03-16 12:26:51 +0100

    feat: add description cropped

---
+++ src/components/base/DescriptionCropped.vue
@@ -0,0 +1,23 @@
+<template>
+  <TipTapOutput :content="content" class="cropped" />
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  content: string
+  // max line to show
+  line: number
+}>()
+</script>
+
+<style lang="scss" scoped>
+.cropped {
+  display: -webkit-box;
+  -webkit-line-clamp: v-bind('line');
+  line-clamp: v-bind('line');
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  word-break: break-word;
+}
+</style>
