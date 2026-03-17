b237387c555427168631b3d1af10894f7e02cb72
rgermain <contact@germainremi.fr>
2026-03-17 13:16:27 +0100

    test: fix slug group

--- tests/unit/pages/GroupPage/GroupPageInnerV2.spec.ts
+++ tests/unit/pages/GroupPage/GroupPageInnerV2.spec.ts
@@ -15,7 +15,7 @@ describe('GroupProjectsTab', () => {
   const orgaCode = useOrganizationCode()
 
   it('Render', async () => {
-    registerEndpoint(`organization/${orgaCode}/people-group/${group.id}/`, () => group)
+    registerEndpoint(`organization/${orgaCode}/people-group/${group.slug}/`, () => group)
     const wrapper = lpiMount(GroupPage, {
       props: {
         group,
@@ -27,7 +27,7 @@ describe('GroupProjectsTab', () => {
   })
 
   it('Error fetch', async () => {
-    registerEndpoint(`organization/${orgaCode}/people-group/${group.id}/`, () => {
+    registerEndpoint(`organization/${orgaCode}/people-group/${group.slug}/`, () => {
       throw createError({ status: 500 })
     })
     const wrapper = lpiMount(GroupPage, {
