74bf2719fe7596dc16881764736b889c40aca1f6
Julien Droulez <julien.droulez@learningplanetinstitute.org>
2026-03-24 16:45:38 +0100

    feat: user consent for porfile and current page access in chatbot

--- src/app/useProjectPagesRoutes.ts
+++ src/app/useProjectPagesRoutes.ts
@@ -6,6 +6,11 @@ export default function useProjectPagesRoutes() {
       name: 'pageProject',
       redirect: { name: 'projectSummary' },
       component: () => import('../pages/ProjectPageV2/ProjectPage.vue'),
+      meta: {
+        chatBotContext: (route) => `
+          This page describe a project whose slug or id is ${route.params.slugOrId}. This is the slugOrId parameter awaited by tools who use projects
+        `,
+      },
       // dont reset scroll here it is done in the component as it handle complex scroll behavior for tabs
       children: [
         {
