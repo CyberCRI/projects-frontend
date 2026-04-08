4e00e51b568141f26d29cec44b11270f42bd6fa3
Julien Droulez <julien.droulez@learningplanetinstitute.org>
2026-04-08 17:39:01 +0200

    feat: skills and tools and agent options

--- src/pages/Chatbot/ChatbotPage.vue
+++ src/pages/Chatbot/ChatbotPage.vue
@@ -8,15 +8,27 @@ const options = { query: { id: props.chatId } }
 const url = '/api/agents/get'
 const { data: agent, status, error, clear } = await useFetch(url, options)
 const loading = useLoadingFromStatus(status)
+
+const hasUserContext = ref(agent.value.useProfileData)
+const hasPageContext = ref(false)
+const {
+  allowProfile,
+  updateAllowProfile,
+  allowCurrentPage,
+  updateAllowCurrentPage,
+  computePageContext,
+  contextMessages,
+} = useChatbotContext({ hasUserContext, hasPageContext })
 </script>
 <template>
   <div class="page-section page-section-narrow page-top">
     <div>
-      <h1 class="page-title">Chat with a bot!</h1>
-      <p>Chat #{{ chatId }} - {{ agent?.title }}</p>
+      <h1 class="page-title">Chat #{{ chatId }} - {{ agent?.title }}</h1>
+      <p>{{ agent.description }}</p>
     </div>
+    <ChatbotOptions :has-user-context="hasUserContext" />
     <ClientOnly>
-      <Chatbot :endpoint="CHAT_ENDPOINT" :context-messages="[]" />
+      <Chatbot :endpoint="CHAT_ENDPOINT" :context-messages="contextMessages" />
     </ClientOnly>
   </div>
 </template>
