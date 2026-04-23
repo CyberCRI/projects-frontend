3733e636dd82021480801cef662650c77cba053c
rgermain <contact@germainremi.fr>
2026-04-23 14:14:01 +0200

    test: fix tests goal

--- tests/unit/components/project/goal/GoalDrawer.spec.ts
+++ tests/unit/components/project/goal/GoalDrawer.spec.ts
@@ -1,6 +1,9 @@
 import { lpiShallowMount, lpiMount } from '@/../tests/helpers/LpiMount'
 import GoalDrawer from '@/components/project/goal/GoalDrawer.vue'
-import { ProjectOutputFactory } from '@/../tests/factories/project.factory'
+import {
+  ProjectOutputFactory,
+  TranslatedProjectFactory,
+} from '@/../tests/factories/project.factory'
 import { OrganizationOutputFactory } from '@/../tests/factories/organization.factory'
 import { beforeEach, describe, expect, it } from 'vitest'
 import pinia from '@/stores'
@@ -9,32 +12,34 @@ import useProjectsStore from '@/stores/useProjects'
 
 describe('GoalDrawer.vue', () => {
   let wrapper
-  let defaultParams
+  let defaultProps
 
   beforeEach(() => {
     const projectsStore = useProjectsStore(pinia)
 
     projectsStore.project = {
-      ...ProjectOutputFactory.generate(),
+      ...TranslatedProjectFactory.generate(),
       files: [],
       links: [],
     }
     const organizationsStore = useOrganizationsStore(pinia)
     organizationsStore._current = OrganizationOutputFactory.generate()
-    defaultParams = {}
+    defaultProps = {
+      project: projectsStore.project,
+    }
   })
   it('should render component', () => {
-    const wrapper = lpiShallowMount(GoalDrawer, defaultParams)
+    const wrapper = lpiShallowMount(GoalDrawer, { props: defaultProps })
     expect(wrapper.exists()).toBe(true)
   })
 
   it('should render GoalDrawer component fully', () => {
-    wrapper = lpiMount(GoalDrawer, defaultParams)
+    wrapper = lpiMount(GoalDrawer, { props: defaultProps })
     expect(wrapper.exists()).toBe(true)
   })
 
   it('should emit the close event', () => {
-    wrapper = lpiMount(GoalDrawer, defaultParams)
+    wrapper = lpiMount(GoalDrawer, { props: defaultProps })
     const vm: any = wrapper.vm
 
     vm.closeModalNoConfirm()
