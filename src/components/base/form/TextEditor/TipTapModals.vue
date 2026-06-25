4bb2bd586e51692f51291922be18905e69e273bb
rgermain <contact@germainremi.fr>
2026-06-25 09:56:08 +0200

    linter

--- src/components/base/form/TextEditor/TipTapModals.vue
+++ src/components/base/form/TextEditor/TipTapModals.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import type { PropsDefinitions } from '~/components/base/form/TextEditor/useTipTap'
-import ColorMenuBar from '~/components/base/form/TextEditor/ColorMenuBar.vue'
 import type { ImageModealCreated } from '~/models/image.model.js'
 import EditorModalVideo from './modals/EditorModalVideo.vue'
 import EditorModalImage from './modals/EditorModalImage.vue'
