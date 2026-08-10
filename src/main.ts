2106a6acc11d7a1fbcdb15e7e0b4fad0e2a26412
Julien Droulez <julien.droulez@learningplanetinstitute.org>
2026-08-10 17:35:40 +0200

    fix: revert bad fix of api init

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,10 @@ import initSentry from '~/app/initSentry'
 import initUser from '~/app/initUser'
 import analytics from '~/analytics'
 
-// initialize client apis with projects-frontend options/config
-initializeClientApi()
 // quick redirect to keycloak login if url says so
 if (import.meta.client) {
+  // initialize client apis with projects-frontend options/config
+  initializeClientApi()
   quickLogin()
 
   // bug fix for leaflet's marker
