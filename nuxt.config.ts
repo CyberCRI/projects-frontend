a00c364157f75f47c84272873a3ecf765e95cc35
Julien Droulez <julien.droulez@learningplanetinstitute.org>
2026-09-17 15:54:50 +0200

    chore: temp deactivation of comments

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -250,6 +250,7 @@ export default defineNuxtConfig({
       appHasChatbotPromptDb: 0,
       appLlmModelSuggestions: '',
       appMapRendererApiKey: '',
+      appNewCommentEnabled: 0,
     },
   },
   i18n: {
